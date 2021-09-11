406292a845b67f0fc70ed88641cd6d3409dbd3cd
Gwen Bradbury <charmed_89@msn.com>
2021-09-11 12:07:05 +0000

    Created forum model, views and urls

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Forum(models.Model):
+    image_url = models.URLField(max_length=1024, default="", blank=True)
+    image = models.ImageField(default="", blank=True)
+    forum_name = models.CharField(max_length=100)
+    forum_description = models.TextField()
+    forum_date_created = models.DateTimeField(default=timezone.now)
+    forum_creator = models.ForeignKey(User, default="", blank=True,
+                                      on_delete=models.CASCADE,
+                                      related_name='forum')
+
+    class Meta:
+        ordering = ['-forum_date_created']
+
+    def __str__(self):
+        return self.forum_name
+
+
+class Comment(models.Model):
+    forum_id = models.ForeignKey('Forum', null=True,
+                                 related_name="comments", blank=True,
+                                 on_delete=models.SET_NULL)
+    comment_title = models.CharField(max_length=50)
+    comment_content = models.TextField(max_length=500)
+    comment_date_posted = models.DateTimeField(default=timezone.now)
+    comment_author = models.ForeignKey(User, null=True,
+                                       blank=True,
+                                       on_delete=models.SET_NULL,
+                                       related_name='comment')
+
+    class Meta:
+        ordering = ['-comment_date_posted']
+
+    def __str__(self):
+        return self.comment_title
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,177 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render,
+    redirect,
+    reverse,
+    get_object_or_404
+)
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Forum, Comment
+from .forms import CommentForm, AddForumForm
+
+
+def view_forum(request):
+    """ Returns forum.html """
+    context = {
+        'forum': Forum.objects.all()
+    }
+    return render(request, 'blog/blog.html', context)
+
+
+@login_required
+def forum_comments(request, forum_id):
+    """ Returns forum_comments.html """
+    forum = get_object_or_404(Forum, pk=forum_id)
+    comments = forum.comments.filter()
+    new_comment = None
+    template = 'forum/forum_comments.html'
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            """ Create Comment """
+            new_comment = comment_form.save(commit=False)
+            """ Assign Author To Comment """
+            new_comment.comment_author = request.user
+            new_comment.save()
+            """ Assign Comment to Forum """
+            new_comment.forum_id = forum
+            new_comment.save()
+            comment_form = CommentForm()
+            messages.success(request, 'Successfully posted your comment.')
+            return redirect(reverse('forum_comments', args=[forum.id]))
+    else:
+        comment_form = CommentForm()
+
+    context = {
+        'forum': forum,
+        'comments': comments,
+        'comment_form': comment_form,
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_comment(request, comment_id):
+    """ Edit Comment """
+    """ Prefill Form """
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.user == comment.comment_author or request.user.is_superuser:
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST, instance=comment)
+            if comment_form.is_valid():
+                comment_form.save()
+                messages.success(request, 'Comment successfully updated')
+                return redirect(reverse('view_forum'))
+            else:
+                messages.error(request,
+                               'Error - Please check form is valid and \
+                                    try again.')
+        else:
+            comment_form = CommentForm(instance=comment)
+            messages.info(request, f'You are editing {comment.comment_title}')
+    else:
+        messages.error(request, 'Sorry, only the comment author can do that')
+        return redirect(reverse('home'))
+
+    template = 'forum/edit_comment.html'
+    context = {
+        'comment_form': comment_form,
+        'comment': comment,
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.user == comment.comment_author or request.user.is_superuser:
+        """ Delete Comment """
+        comment.delete()
+        messages.success(request, 'Comment deleted')
+        return redirect(reverse('view_forum'))
+    else:
+        messages.error(request, 'Sorry, only the comment author can do that')
+        return redirect(reverse('home'))
+
+
+@login_required
+def add_forum(request):
+    """ Superuser Access Only """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, owners only function')
+        return redirect(reverse('home'))
+    """ Add Forum """
+    if request.method == 'POST':
+        add_forum_form = AddForumForm(request.POST, request.FILES)
+        if add_forum_form.is_valid():
+            new_forum = add_forum_form.save(commit=False)
+            """ Assign Creator To Forum """
+            new_forum.forum_creator = request.user
+            new_forum.save()
+            add_forum_form = AddForumForm()
+            messages.success(request, 'Forum successfully added')
+            return redirect(reverse('forum_comments', args=[new_forum.id]))
+        else:
+            messages.error(request,
+                           'Error - Please check form is valid and try again.')
+    else:
+        add_forum_form = AddForumForm()
+
+    template = 'blog/add_forum.html'
+    context = {
+        'add_forum_form': add_forum_form,
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_forum(request, forum_id):
+    """ Superuser Access Only """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, owners only function')
+        return redirect(reverse('home'))
+    """ Edit Forum """
+    """ Prefill Form """
+    forum = get_object_or_404(Forum, pk=forum_id)
+    if request.method == 'POST':
+        add_forum_form = AddForumForm(request.POST, request.FILES, instance=forum)
+        if add_forum_form.is_valid():
+            add_forum_form.save()
+            messages.success(request, 'Post successfully updated')
+            return redirect(reverse('forum_comments', args=[forum.id]))
+        else:
+            messages.error(request,
+                           'Error - Please check form is valid and try again.')
+    else:
+        add_forum_form = AddForumForm(instance=forum)
+        messages.info(request, f'You are editing {forum.forum_name}')
+
+    template = 'blog/edit_post.html'
+    context = {
+        'add_forum_form': add_forum_form,
+        'forum': forum,
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_forum(request, forum_id):
+    """ Superuser Access Only """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, owners only function')
+        return redirect(reverse('home'))
+    """ Delete Forum """
+    forum = get_object_or_404(Forum, pk=forum_id)
+    forum.delete()
+    messages.success(request, 'Forum deleted')
+    return redirect(reverse('view_forum'))
