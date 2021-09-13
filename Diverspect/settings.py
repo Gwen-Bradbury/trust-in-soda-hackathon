c16379915a9bcb07fa935f1b0563bb7051bdbe9b
Gwen Bradbury <charmed_89@msn.com>
2021-09-13 08:38:26 +0000

    Deployment to heroku

--- Diverspect/settings.py
+++ Diverspect/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = os.environ.get('DIVERSPECT_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['trust-in-soda-diverspect.herokuapp.com', 'localhost']
 
 # Application definition
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Diverspect.wsgi:application
