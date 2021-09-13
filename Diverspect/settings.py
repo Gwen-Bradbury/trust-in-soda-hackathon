de42dce6d66825cc759a5a9ab7044694c7c0b771
Gwen Bradbury <61613653+Gwen-Bradbury@users.noreply.github.com>
2021-09-13 12:05:39 +0100

    Fixed typo in settings.py (#76)

--- Diverspect/settings.py
+++ Diverspect/settings.py
@@ -173,7 +173,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'ginporium-ms4'
+    AWS_STORAGE_BUCKET_NAME = 'trust-in-soda-diverspect'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
