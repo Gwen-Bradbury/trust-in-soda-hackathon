e4853cc14a24a3df05519d4dfaaab434183b79e3
Gwen Bradbury <charmed_89@msn.com>
2021-09-13 08:56:03 +0000

    Set debug

--- Diverspect/settings.py
+++ Diverspect/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DIVERSPECT_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['trust-in-soda-diverspect.herokuapp.com', 'localhost']
 
